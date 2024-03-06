73b78cd1e5efd8aac88e3e0897a82c0312bd3282
Raiyan109 <kraiyan109@gmail.com>
2024-03-06 15:34:49 +0600

    ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const cors = require('cors');
+const { default: axios } = require('axios');
 require('dotenv').config()
 
 const app = express()
@@ -8,6 +8,29 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/', async (req, res) => {
+    try {
+        const city = req.query.city || 'dhaka'; // Default to 'dhaka' if no city is provided
+        const data = await fetchData(city);
+        console.log(data);
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+
+})
+
+
+async function fetchData(city) {
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 // mongoose.set("strictQuery", false);
 // mongoose.connect(process.env.MONGO_URI)
@@ -15,7 +38,7 @@ app.use(cors())
 app.listen(process.env.PORT, (req, res) => {
     console.log(`Server listening on ${process.env.PORT}`)
 })
-    // })
-    // .catch((error) => {
-    //     console.log(error)
-    // })
+// })
+// .catch((error) => {
+//     console.log(error)
+// })
